bd8a56800ed3488827c681d5d30590a8cc46a1ab
Mario Villatoro <Mario Villatoro>
2017-02-06 13:47:37 -0600

    d finished, nfa ui finished, test pending

--- Automata.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by mvill on 1/31/2017.
- */
-
-function Automata(automataType) {
-    this.automataType = automataType;
-}
--- DrawAutomata.js
+++ DrawAutomata.js
@@ -2,17 +2,6 @@
  * Created by mvill on 1/27/2017.
  */
 
-
-var nodeIds, nodes, edges, network;
-
-var states = [];
-
-var transitions = [];
-
-nodeIds = [];
-edges = [];
-nodes = new vis.DataSet();
-
 //region Objects
 
 function State(name, type, stateId) {
@@ -29,22 +18,50 @@ function Transition(transitionChar, originState, nextState) {
 
 //endregion
 
-//region Create functions
+//region DFA
 
-function createNewState(stateType) {
+var nodeIds, nodes, edges, network;
 
-    var stateName = prompt("State name", "State");
+var states = [];
 
-    if(stateName != null){
-        if(stateType == "IF")
-            createState(stateName,"IF", "#00ff99"); //INITIAL FINAL
-        else if(stateType == "F")
-            createState(stateName,"F", "#00ff99"); //FINAL
-        else if(stateType == "I")
-            createState(stateName,"I", "#66a3ff"); //INITIAL
-        else if(stateType == "N")
-            createState(stateName,"N", "#808080"); //NORMAL
+var transitions = [];
+
+nodeIds = [];
+edges = [];
+nodes = new vis.DataSet();
+
+function createNewState() {
+
+    var shiftKey = false;
+    var ctrlKey = false;
+    var altKey = false;
+
+    if(event.shiftKey && event.ctrlKey)
+        shiftKey = ctrlKey = true;
+    if (event.shiftKey)
+        shiftKey = true;
+    else if(event.ctrlKey)
+        ctrlKey = true;
+    else if(event.altKey)
+        altKey = true;
+
+    if(shiftKey || ctrlKey || altKey){
+
+        var stateName = prompt("State name", "State");
+
+        if(stateName != null){
+            if(shiftKey && ctrlKey)
+                createState(stateName,"IF", "#00ff99"); //INITIAL FINAL
+            else if(shiftKey)
+                createState(stateName,"F", "#00ff99"); //FINAL SHIFT
+            else if(ctrlKey)
+                createState(stateName,"I", "#66a3ff"); //INITIAL CONTROL
+            else if(altKey)
+                createState(stateName,"N", "#808080"); //NORMAL ALT
+        }
     }
+
+
 }
 
 function createState(stateName, stateType, colorType) {
@@ -57,30 +74,6 @@ function createState(stateName, stateType, colorType) {
         callSnackbar("State " + newState.stateName + " created");
     }else
         callSnackbar("State " + stateName + " already exists...");
-}
-
-function createNewTransition() {
-
-    var transitionData = prompt("Transition", "0,a,b");
-
-    if(transitionData != null){
-        var dataArray = transitionData.split(",");
-        var originState = getState(dataArray[1]);
-        var nextState = getState(dataArray[2]);
-
-        createTransition(dataArray[0], originState, nextState);
-    }
-}
-
-function createTransition(transitionChar, originState, nextState)       {
-
-    if(originState != null &&  nextState != null){
-            transitions.push(new Transition(transitionChar, originState, nextState));
-            createEdge(transitionChar, originState, nextState);
-            callSnackbar("Char: " + transitionChar + " Origin: " + originState.stateName + " Next: " + nextState.stateName);
-    }else{
-        callSnackbar("Invalid input");
-    }
 }
 
 function addNode(stateId, stateName, color) {
@@ -110,9 +103,43 @@ function createEdge(transitionChar, originState, nextState) {
     init();
 }
 
-//endregion
+function stateExist(stateName) {
+    for(var i = 0; i < states.length; i++)
+        if(states[i].stateName == stateName)
+            return true;
 
-//region DFA
+    return false;
+}
+
+function createNewTransition() {
+
+    var transitionData = prompt("Transition", "0,a,b");
+
+    if(transitionData != null){
+        var dataArray = transitionData.split(",");
+        var originState = getState(dataArray[1]);
+        var nextState = getState(dataArray[2]);
+
+        createTransition(dataArray[0], originState, nextState);
+    }
+}
+
+function createTransition(transitionChar, originState, nextState)       {
+
+    if(originState != null &&  nextState != null){
+        transitions.push(new Transition(transitionChar, originState, nextState));
+        createEdge(transitionChar, originState, nextState);
+        callSnackbar("Char: " + transitionChar + " Origin: " + originState.stateName + " Next: " + nextState.stateName);
+    }else{
+        callSnackbar("Invalid input");
+    }
+}
+
+function getState(stateName) {
+    for(var i = 0; i < states.length; i++)
+        if(states[i].stateName == stateName)
+            return states[i];
+}
 
 function reverse(s){
     return s.split("").reverse().join("");
@@ -121,11 +148,6 @@ function reverse(s){
 function acceptsString(testString) {
 
     var newTestString = reverse(testString);
-
-    console.log("Begin accept string {");
-    console.log("   initial state: " + states[0].stateName);
-    console.log("   state type: " + states[0].stateType);
-    console.log("   test string: " + newTestString);
 
     var lastState = transitionFunction(states[0], newTestString);
 
@@ -138,52 +160,28 @@ function acceptsString(testString) {
         alert("The string was NOT accepted :(");
     }
 
-    console.log("}End accept string");
-
 }
 
 function transitionFunction(initialState, testString) {
 
-    console.log("   Begin trans func{");
-    console.log("        initial state: " + initialState.stateName);
-    console.log("        state type: " + initialState.stateType);
-    console.log("        test string: " + testString);
-
     var testChar = testString.charAt(0);
-
-    console.log("        test 1st char: " + testChar);
 
     var lastState;
     if (testString.length > 1) {
         var newTestString =  testString.slice(1,testString.length);
-        console.log("       test 1st if new test string: " + newTestString);
-
         lastState = extendedFunctionTransition(transitionFunction(initialState, newTestString), testChar);
 
     }else
     {
-        console.log("        test 2st if new test char: " + testChar);
-
         lastState =extendedFunctionTransition(initialState, testChar);
     }
-
-    console.log("       }End trans func");
 
     return lastState;
 }
 
 function extendedFunctionTransition(state, testChar) {
 
-    console.log("           Begin ext func{");
-
-    console.log("               test char: " + testChar);
-
-    console.log("               state name: " + state.stateName);
-    console.log("               state type: " + state.stateType);
-
     var nextState = getNextState(state, testChar);
-
-    console.log("               next state: " + nextState[0].stateName);
 
     if(nextState[0] != null){
         console.log("           returns state name: " + nextState[0].stateName);
@@ -191,45 +189,8 @@ function extendedFunctionTransition(state, testChar) {
         return nextState[0];
     }
 
-    console.log("           }End ext func");
     return null;
 
-}
-
-function getNextState(state, transitionChar) {
-
-    console.log("               Begin get next state{");
-    console.log("                   state name: " + state.stateName);
-    console.log("                   transitions length: " + transitions.length);
-
-    var nextStates = [];
-    for(var i = 0; i< transitions.length; i++){
-
-        console.log("                   Transition " + i + " origin state :" + transitions[i].originState.stateName);
-        console.log("                   Transition " + i + " next state :" + transitions[i].nextState.stateName);
-        console.log("                   Transition " + i + " char trans :" + transitions[i].transitionChar);
-
-        if(transitions[i].originState.stateName == state.stateName && transitions[i].transitionChar == transitionChar){
-
-            console.log("                   transition match");
-
-            // if(transitions[i].nextState != null){
-
-            // console.log("State exists");
-            var nextState = transitions[i].nextState;
-            console.log(nextState);
-            nextStates.push(nextState);
-            console.log(nextStates);
-            //}
-        }
-    }
-
-    //console.log("                   return next states: " + nextState.stateName);
-    console.log(nextStates);
-
-    console.log("               }End get next state");
-
-    return nextStates;
 }
 
 function init() {
@@ -242,20 +203,17 @@ function init() {
     network = new vis.Network(container, data, options);
 }
 
-function getState(stateName) {
-    for(var i = 0; i < states.length; i++){
-        if(states[i].stateName == stateName){
-            return states[i];
+function getNextState(state, transitionChar) {
+
+    var nextStates = [];
+    for(var i = 0; i< transitions.length; i++){
+
+        if(transitions[i].originState.stateName == state.stateName && transitions[i].transitionChar == transitionChar){
+            var nextState = transitions[i].nextState;
+            nextStates.push(nextState);
         }
     }
-
-}
-
-function callSnackbar(displayText) {
-    var x = document.getElementById("snackbar");
-    x.className = "show";
-    document.getElementById("snackbar").innerHTML = displayText;
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    return nextStates;
 }
 
 function load(evt) {
@@ -275,30 +233,224 @@ function save(filename, text) {
     document.body.removeChild(element);
 }
 
-function stateExist(stateName) {
-    for(var i = 1; i < states.length; i++)
-        if(states[i].stateName == stateName)
+//endregion
+
+
+//region NFA
+
+var nodeIdsNfa, nodesNfa, edgesNfa, networkNfa;
+
+var statesNfa = [];
+
+var transitionsNfa = [];
+
+nodeIdsNfa = [];
+edgesNfa = [];
+nodesNfa = new vis.DataSet();
+
+function createNewStateNfa() {
+
+    var shiftKey = false;
+    var ctrlKey = false;
+    var altKey = false;
+
+    if(event.shiftKey && event.ctrlKey)
+        shiftKey = ctrlKey = true;
+    if (event.shiftKey)
+        shiftKey = true;
+    else if(event.ctrlKey)
+        ctrlKey = true;
+    else if(event.altKey)
+        altKey = true;
+
+    if(shiftKey || ctrlKey || altKey){
+
+        var stateName = prompt("State name", "State");
+
+        if(stateName != null){
+            if(shiftKey && ctrlKey)
+                createStateNfa(stateName,"IF", "#00ff99"); //INITIAL FINAL
+            else if(shiftKey)
+                createStateNfa(stateName,"F", "#00ff99"); //FINAL SHIFT
+            else if(ctrlKey)
+                createStateNfa(stateName,"I", "#66a3ff"); //INITIAL CONTROL
+            else if(altKey)
+                createStateNfa(stateName,"N", "#808080"); //NORMAL ALT
+        }
+    }
+}
+
+function createStateNfa(stateName, stateType, colorType) {
+
+    if(!stateExistNfa(stateName)){
+        var stateId = (Math.random() * 1e7).toString(32);
+        var newState = new State(stateName, stateType, stateId);
+        statesNfa.push(newState);
+        addNodeNfa(stateId,stateName, colorType);
+        callSnackbar("State " + newState.stateName + " created");
+    }else
+        callSnackbar("State " + stateName + " already exists...");
+}
+
+function createNewTransitionNfa() {
+
+    var transitionData = prompt("Transition", "0,a,b");
+
+    if(transitionData != null){
+        var dataArray = transitionData.split(",");
+        var originState = getStateNfa(dataArray[1]);
+        var nextState = getStateNfa(dataArray[2]);
+
+        createTransitionNfa(dataArray[0], originState, nextState);
+    }
+}
+
+function createTransitionNfa(transitionChar, originState, nextState)       {
+
+    if(originState != null &&  nextState != null){
+        transitionsNfa.push(new Transition(transitionChar, originState, nextState));
+        createEdgeNfa(transitionChar, originState, nextState);
+        callSnackbar("Char: " + transitionChar + " Origin: " + originState.stateName + " Next: " + nextState.stateName);
+    }else{
+        callSnackbar("Invalid input");
+    }
+}
+
+function addNodeNfa(stateId, stateName, color) {
+    nodesNfa.add({id:stateId, label:stateName, color: {background:color, border:'black'}});
+    nodeIdsNfa.push(stateId);
+    initNfa();
+}
+
+function createEdgeNfa(transitionChar, originState, nextState) {
+
+    var transitionId = (Math.random() * 1e7).toString(32);
+    edgesNfa.push({
+        id: transitionId,
+        from: originState.stateId,
+        to: nextState.stateId,
+        label: transitionChar,
+        color:{color:'rgba(30,30,30,0.2)', highlight:'blue'},
+        arrows:'to'
+    });
+
+    initNfa();
+}
+
+function initNfa() {
+    var container = document.getElementById('myNfaDiagramDiv');
+    var data = {
+        nodes: nodesNfa,
+        edges: edgesNfa
+    };
+    var options = {};
+    networkNfa = new vis.Network(container, data, options);
+}
+
+function getStateNfa(stateName) {
+    for(var i = 0; i < statesNfa.length; i++)
+        if(statesNfa[i].stateName == stateName)
+            return statesNfa[i];
+}
+
+function acceptsStringNfa(testString) {
+
+    var accepts = false;
+    var lastStates = transitionFunctionNfa(statesNfa[0], testString);
+
+//    for(var  i = 0; i < lastStates.length; i++)
+        if(lastStates == "F" || lastStates == "IF" )
+            accepts = true;
+
+    if(accepts)
+        alert("String was accepted :D");
+    else
+        alert("The string was NOT accepted :(");
+
+}
+
+function transitionFunctionNfa(initialState, testString){
+
+    var lastStates;
+    var returnStates;
+    var testChar =  testString.charAt(0);
+
+    if(testString > 1){
+        var newTestString =  testString.slice(1,testString.length);
+        lastStates = extendedFunctionTransitionNfa(transitionFunctionNfa(initialState, newTestString), testChar);
+
+        for(var i = 0; i < lastStates.length; i++){
+            returnStates = transitionFunctionNfa(lastStates[i], newTestString);
+        }
+
+    }else{
+        lastStates = extendedFunctionTransition(initialState, testChar);
+        for(var j = 0; j < lastStates.length; j++){
+            returnStates = transitionFunctionNfa(lastStates[j], testChar);
+        }
+    }
+
+    return returnStates;
+
+}
+
+function extendedFunctionTransitionNfa(state, testChar) {
+
+    var nextStates = getNextStatesNfa(state, testChar);
+
+    if(nextStates != null){
+        return nextStates;
+    }
+
+    return null;
+}
+
+function getNextStatesNfa(state, transitionChar){
+
+    var nextStates = [];
+    for(var i = 0; i< transitionsNfa.length; i++){
+
+        if(transitionsNfa[i].originState.stateName == state.stateName && transitionsNfa[i].transitionChar == transitionChar){
+
+            var nextState = transitionsNfa[i].nextState;
+            nextStates.push(nextState);
+        }
+    }
+
+    return nextStates;
+}
+
+
+function stateExistNfa(stateName) {
+    for(var i = 0; i < statesNfa.length; i++)
+        if(statesNfa[i].stateName == stateName)
             return true;
 
     return false;
 }
 
-function printStates() {
-
-    for (var i = 0; i < states.length; i++) {
-        console.log("State name: " + states[i].stateName);
-        console.log("State type: " + states[i].stateType);
-    }
-}
-
-function printTransitions() {
-    for(var i = 0; i < transitions.length; i++){
-        console.log(transitions[i].transitionChar);
-        console.log(transitions[i].originState);
-        console.log(transitions[i].nextState);
-    }
-}
 
 //endregion
 
 
+//region Global Functions
+
+function callSnackbar(displayText) {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    document.getElementById("snackbar").innerHTML = displayText;
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
+function hideShow(divID) {
+
+    var dfaDiv = document.getElementById(divID);
+
+    if(dfaDiv.style.display == 'block')
+        dfaDiv.style.display = 'none';
+    else
+        dfaDiv.style.display = 'block';
+
+}
+
+//endregion
